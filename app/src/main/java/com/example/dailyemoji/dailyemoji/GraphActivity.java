4f945865e686979193e3ffbd9b963dace3f9e41a
georgenjoo <georgenjoo@yahoo.com>
2017-09-16 20:18:32 -0700

    updated added button for month and year

--- app/src/main/java/com/example/dailyemoji/dailyemoji/GraphActivity.java
+++ app/src/main/java/com/example/dailyemoji/dailyemoji/GraphActivity.java
@@ -1,7 +1,10 @@
 package com.example.dailyemoji.dailyemoji;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -14,6 +17,9 @@ public class GraphActivity extends AppCompatActivity {
 
     private LineGraphSeries<DataPoint> thingsToPlot;
     private List<Integer> listOfY;
+    public String topText;
+    private TextView tv;
+    private GraphView gv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +29,35 @@ public class GraphActivity extends AppCompatActivity {
 
         thingsToPlot = new LineGraphSeries<DataPoint>();
         listOfY = new ArrayList<Integer>();
+        topText = "";
 
-        updateThingsToPlot();
+        updateThingsToPlot(true);
         drawGraph();
+
+        tv = (TextView) findViewById(R.id.textView1);
+        tv.setText(getTopText());
 
     }
 
+    private void setTopText(String nam){
+        this.topText = nam;
+    }
 
-    public void updateThingsToPlot(){
+    public String getTopText(){
+        return this.topText;
+    }
+
+
+    public void updateThingsToPlot(boolean isMonth){
+
+        if (isMonth == true){
+            setTopText("Past Month's Mood Ratings");
+        } else
+            setTopText("Past Year's Mood Ratings");
+
+        listOfY = new ArrayList<Integer>();
+        thingsToPlot = new LineGraphSeries<DataPoint>();
+
         listOfY.add(1);
         listOfY.add(2);
         listOfY.add(3);
@@ -57,13 +84,29 @@ public class GraphActivity extends AppCompatActivity {
         listOfY.add(10);
         listOfY.add(4);
 
+    }
+
+    public void click30(View view){
+
+        updateThingsToPlot(true);
+        setTopText("Past Month's Mood Ratings");
+        tv.setText(getTopText());
+        drawGraph();
+    }
+
+    public void click365(View view){
+        updateThingsToPlot(false);
+        setTopText("Past Year's Mood Ratings");
+        tv.setText(getTopText());
+        drawGraph();
     }
 
 
     public void drawGraph(){
-        GraphView gv = (GraphView) findViewById(R.id.graph);
 
-        gv.setTitle("Mood Graph");
+
+        gv = (GraphView) findViewById(R.id.graph);
+
         int x=-1,y =0;
 
         for (int i = 0 ; i <listOfY.size() ; i++){
@@ -73,6 +116,7 @@ public class GraphActivity extends AppCompatActivity {
         }
 
         gv.addSeries(thingsToPlot);
+
     }
 
 
