629b0b26aca2c420a4665e8624c58812dd46a477
minderov <coucoueoeu@gmail.com>
2017-09-17 11:35:00 -0700

    Added notifications

--- app/src/main/java/com/example/dailyemoji/dailyemoji/MainActivity.java
+++ app/src/main/java/com/example/dailyemoji/dailyemoji/MainActivity.java
@@ -1,12 +1,22 @@
 package com.example.dailyemoji.dailyemoji;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
@@ -38,9 +48,17 @@ public class MainActivity extends Activity {
                 selfRateButton.setVisibility(View.VISIBLE);
             }
         });
+
+        MyTimerTask myTask = new MyTimerTask();
+        Timer myTimer = new Timer();
+
+        myTimer.schedule(myTask, 5000, 2 * 60 * 1000);
     }
 
     public void clickRating(View view){
+        // SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        // String freq = prefs.getString("notifications_frequency", "DEFAULT");
+
 //        DBHandler db = new DBHandler(this);
 //
 //        Rating rating = new Rating();
@@ -78,5 +96,31 @@ public class MainActivity extends Activity {
     public void clickHistory(View view){
         Intent intent = new Intent(this, HistoryActivity.class);
         startActivity(intent);
+    }
+
+    private class MyTimerTask extends TimerTask {
+        public void run() {
+            generateNotification(getApplicationContext(), "What's your mood?");
+        }
+    }
+
+    private void generateNotification(Context context, String message) {
+
+        int icon = R.drawable.emoji_1f914;
+        String appname = context.getResources().getString(R.string.app_name);
+        NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification notification;
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, MainActivity.class), 0);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                    context);
+            notification = builder.setContentIntent(contentIntent)
+                    .setSmallIcon(icon).setTicker(appname).setWhen(0)
+                    .setAutoCancel(true).setContentTitle(appname)
+                    .setContentText(message).build();
+
+            notificationManager.notify(0, notification);
     }
 }
